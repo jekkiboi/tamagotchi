9c1dc6906dc34f203007c899aa67ce07161076e8
jekkiboi <hollymayer4@gmail.com>
2021-07-21 09:58:12 -0700

    moved age counter

--- app.js
+++ app.js
@@ -1,57 +1,7 @@
-class Tamagotchi {
-    constructor(name, age, hunger, sleepiness, boredom){
-        this.name = name;
-        this.age = age;
-        this.hunger = hunger;
-        this.sleepiness = sleepiness;
-        this.boredom = boredom;
-    }
-    greet(){
-    console.log('hello master!')
-    }
-}
-
-class Player extends Tamagotchi{
-    constructor(name, age, hunger, sleepiness, boredom){
-        super(name, age, hunger, sleepiness, boredom);
-        this.characteristics = ['silly', 'cute', 'wild', 'adorable']
-    }
-    
-hungryTam(){
-    if(this.tamagatchi.hunger > 6){
-        console.log('Uh oh! Time to feed Tamagotchi!')
-    }else {
-        console.log(${this.name} + ` doesn't need feeding right now`)
-    }
-}
-sleepyTam(){
-    if(this.tamagatchi.sleepiness > 6){
-    console.log('Uh oh! Time to go to bed...')
-    }else {
-    console.log(${this.name} + ` is fully rested`)
-    }
-}
-boredTam(){
-    if(this.tamagatchi.boredom > 6){
-    console.log('Your Tamagotchi is bored!')
-    }else {
-    console.log(${this.name} + ` is ready to rumble!`)
-        }
-    }
-}
-
-const feedrEl = document.getElementById('btn-feed');
-const sleepEl = document.getElementById('btn-sleep');
-const playEl = document.getElementById('btn-play');
-
-const hungerEl = document.getElementById('hunger');
-const pauseEl = document.getElementById('pause');
-const boredomEl = document.getElementById('boredom');
-
 ///////AGE COUNTER
-const startEl = document.getElementById('btn-start');
-const pauseEl = document.getElementById('btn-pause');
-const resetEl = document.getElementById('btn-reset');
+const startEl = document.getElementById('btn-startGame');
+const pauseEl = document.getElementById('btn-pauseGame');
+const resetEl = document.getElementById('btn-resetGame');
 const ageEl = document.getElementById('age');
 
 
@@ -77,7 +27,67 @@ ageEl.textContent = 'Age: ' + age;
     //stop the interval from running
     clearInterval(interval);
 }
+
 //When the start button is clicked, call the provided function
 startEl.addEventListener('click', handleStartClick);
 pauseEl.addEventListener('click', handlePauseClick);
 resetEl.addEventListener('click', handleResetClick);
+
+
+// class Tamagotchi {
+//     constructor(name, age, hunger, sleepiness, boredom){
+//         this.name = name;
+//         this.age = age;
+//         this.hunger = hunger;
+//         this.sleepiness = sleepiness;
+//         this.boredom = boredom;
+//     }
+//     greet(){
+//     console.log('hello master!')
+//     }
+// }
+
+// class Player extends Tamagotchi{
+//     constructor(name, age, hunger, sleepiness, boredom){
+//         super(name, age, hunger, sleepiness, boredom);
+//         this.characteristics = ['silly', 'cute', 'wild', 'adorable']
+//     }
+
+
+// // hungryTam(){
+// //     if(this.tamagatchi.hunger > 6){
+// //         console.log('Uh oh! Time to feed Tamagotchi!')
+// //     }else {
+// //         console.log(${this.name} + ` doesn't need feeding right now`)
+// //     }
+// // }
+// // sleepyTam(){
+// //     if(this.tamagatchi.sleepiness > 6){
+// //     console.log('Uh oh! Time to go to bed...')
+// //     }else {
+// //     console.log(${this.name} + ` is fully rested`)
+// //     }
+// // }
+// // boredTam(){
+// //     if(this.tamagatchi.boredom > 6){
+// //     console.log('Your Tamagotchi is bored!')
+// //     }else {
+// //     console.log(${this.name} + ` is ready to rumble!`)
+// //         }
+// //     }
+// // }
+
+
+
+
+// //////////////////////////////////////////////////////
+
+// //HUNGER COUNTER
+
+// const feedEl = document.getElementById('btn-feed');
+// const sleepEl = document.getElementById('btn-sleep');
+// const playEl = document.getElementById('btn-play');
+
+// const hungerEl = document.getElementById('hunger');
+// const pauseEl = document.getElementById('pause');
+// const boredomEl = document.getElementById('boredom');
